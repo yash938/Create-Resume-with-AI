c62c9bff7b25d84c1cc2edeb75ae3a43488f3be9
Yash Sharma <yash32860@gmail.com>
2025-02-05 21:41:23 +0530

    this is my first ai project

---
+++ create-resume-with-ai/src/main/java/com/ai_resume/CreateResumeWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.ai_resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateResumeWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateResumeWithAiApplication.class, args);
+	}
+
+}
---
+++ create-resume-with-ai/src/test/java/com/ai_resume/CreateResumeWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_resume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateResumeWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
